09044440dd89bb639991114615d333b2a1484831
shinkhant17304 <shinekhant69444@gmail.com>
2026-09-08 01:39:54 +0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -16,7 +16,7 @@ bot.onText(/\/start/, (msg) => {
             keyboard: [[
                 {
                     text: "🛍️ Open Shop",
-                    web_app: { url: "https://your-app-domain.vercel.app" } // သင့် Vercel Link အစားထိုးပါ
+                    web_app: { url: "https://github.com/shinkhant17304/O_shop_project.git" } // သင့် Vercel Link အစားထိုးပါ
                 }
             ]],
             resize_keyboard: true
